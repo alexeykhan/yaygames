b48487177f68bc9e1bb06dc805c2a54832460d67
alexeykhan <git@alexeykhan.ru>
2021-10-22 20:12:33 +0300

    Problem #1. Get Merkle Proof

---
+++ cmd/merkleproof/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/miguelmota/go-solidity-sha3"
+
+	"github.com/alexeykhan/yaygames/pkg/merkle"
+)
+
+// TreeData describes structure of given source file.
+type TreeData struct {
+	Nodes []struct {
+		Address  string `json:"address"`
+		Category uint64 `json:"category"`
+		Amount   uint64 `json:"amount"`
+	} `json:"nodes"`
+}
+
+func main() {
+	const path = "./src/mercleTreeData.json"
+
+	if err := printMerkleProof(path); err != nil {
+		log.Printf("parse data: %v", err)
+		os.Exit(1)
+	}
+}
+
+func printMerkleProof(path string) (err error) {
+	var (
+		mercleTreeFile  *os.File
+		mercleTreeBytes []byte
+		data            TreeData
+	)
+
+	if mercleTreeFile, err = os.Open(path); err != nil {
+		return fmt.Errorf("open file: %w", err)
+	}
+	defer func() {
+		if closeErr := mercleTreeFile.Close(); closeErr != nil {
+			if err == nil {
+				err = fmt.Errorf("close file: %w", closeErr)
+			} else {
+				err = fmt.Errorf("close file: %w: %v", closeErr, err)
+			}
+		}
+	}()
+
+	if mercleTreeBytes, err = ioutil.ReadAll(mercleTreeFile); err != nil {
+		return fmt.Errorf("read file data: %v", err)
+	}
+
+	if err = json.Unmarshal(mercleTreeBytes, &data); err != nil {
+		return fmt.Errorf("unmarshal file data: %v", err)
+	}
+
+	leaves := make([][]byte, len(data.Nodes))
+	for i, node := range data.Nodes {
+		category := strconv.FormatUint(node.Category, 10)
+		amount := strconv.FormatUint(node.Amount, 10)
+		leaves[i] = solsha3.SoliditySHA3(
+			[]string{"address", "uint256", "uint256"},
+			[]interface{}{node.Address, category, amount},
+		)
+	}
+
+	tree := merkle.NewTree(leaves, merkle.Options{SortPairs: true})
+	fmt.Println(tree.GetHexProof(leaves[0]))
+
+	return
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/alexeykhan/yaygames
+
+go 1.16
+
+require (
+	github.com/ethereum/go-ethereum v1.10.11 // indirect
+	github.com/miguelmota/go-solidity-sha3 v0.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+)
